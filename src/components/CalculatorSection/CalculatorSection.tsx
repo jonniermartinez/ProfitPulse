0c7d4dfabe3b4a080ca8a067c114b656fe9fc11c
Jonnier <jonnierosjitas@gmail.com>
2023-09-12 12:42:46 -0500

    any

--- src/components/CalculatorSection/CalculatorSection.tsx
+++ src/components/CalculatorSection/CalculatorSection.tsx
@@ -26,7 +26,7 @@ interface Data {
 function CalculatorSection() {
   const [data, setData] = useState(Array<Data>);
 
-  const handleSubmit = (e: unknown) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     const initialAmmount = Number(e.target.initialAmmount.value);
     const yearlyPercentage = Number(e.target.yearlyPercentage.value);
