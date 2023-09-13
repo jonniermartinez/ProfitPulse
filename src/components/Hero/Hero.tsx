ff343570bd5426f02e831ab58ea107e90b5c0b07
Jonnier <jonnierosjitas@gmail.com>
2023-09-13 10:47:50 -0500

    fix: height on the hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -49,7 +49,7 @@ export default function Hero() {
         <section>
           {/* latest relace */}
           {/* <p className="text-center">hola mundo</p> */}
-          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 min-h-screen">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 h-screen max-h-[600px]">
             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
               Calculate Your Earnings with Our Compound Interest Calculator
             </h1>
